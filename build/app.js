27ef5c1af3fff1f3d15450abaa0f84e6629ae14b
Zac Mitton <zacmitton22@gmail.com>
2017-01-26 14:02:09 -0500

    trufflized and fixed web3

--- build/app.js
+++ build/app.js
@@ -5369,46 +5369,32 @@ window.addEventListener('load', function() {
 
  
 
-// var ProviderEngine = require("../node_modules/web3-provider-engine");
-// var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-// var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-// var Web3 = require("web3");
+var Web3 = require("web3")
+var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"))
 
-
-function setStatus(message) {
-  var status = document.getElementById("status");
-  status.innerHTML = message;
-};
-
-function flipACoin(web3Enabled){
-  document.getElementById('coinAnimation').src='images/flip.gif'
-  document.getElementById('outcome').innerHTML='Outcome: ...'
-  setTimeout(getBlockNumber, 2000)//waiting protects against network propagation attack
+window.onload = function() {
+  if (web3.isConnected()){ setTimeout(flipACoin, 2000) }//waiting prevents attacks
+  else{ document.getElementById('outcome').innerHTML='COULD NOT CONNECT TO ETHEREUM' }
 }
-function getBlockNumber(){
+function flipACoin(web3Enabled){
   web3.eth.getBlockNumber(function(e,referenceBlockNumber){
     document.getElementById('outcome').innerHTML='about 15 more seconds...'
-    getBlockHash(referenceBlockNumber+1)
+    getFutureBlockHash(referenceBlockNumber + 1, showCoinSolution)
   })
 }
-function getBlockHash(targetBlockNumber){
+
+function getFutureBlockHash(targetBlockNumber, callback){
   web3.eth.getBlock(targetBlockNumber, function(e,targetBlock){
     if(targetBlock){
-      var outcome;
-      console.log(targetBlock.hash)
-      outcome = parseInt(targetBlock.hash.toString().slice(60), 16)%2==0 ? "HEADS":"TAILS"
-      document.getElementById('coinAnimation').src='images/'+outcome+'.png'
-      document.getElementById('outcome').innerHTML='Outcome: '+outcome
-    }else{// targetBlock not mined yet. Check again in a second
-      setTimeout(function(){getBlockHash(targetBlockNumber)}, 2500)
+      callback(targetBlock.hash)
+    }else{// targetBlock not mined yet. Check again in 2 seconds
+      setTimeout(function(){getFutureBlockHash(targetBlockNumber, callback)}, 2000)
     }
   })
 }
-
-
-window.onload = function() {
-  console.log(web3.currentProvider)
-  flipACoin()
+var showCoinSolution = function(sha3){
+  console.log("blockhash:", sha3)
+  var outcome = parseInt(sha3.toString().slice(60), 16)%2==0 ? "HEADS":"TAILS"
+  document.getElementById('coinAnimation').src='images/'+outcome+'.png'
+  document.getElementById('outcome').innerHTML='Outcome: '+outcome
 }
-
-function refreshGameData() {}
